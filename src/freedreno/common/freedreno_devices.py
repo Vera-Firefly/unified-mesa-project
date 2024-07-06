d6638d46cd031ffa83036810d163893955664b3c
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2024-07-06 09:02:38 +0800

    Update freedreno_devices.py

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -802,6 +802,11 @@ a7xx_730 = A7XXProps(
     )
 
 a7xx_735 = A7XXProps(
+        stsc_duplication_quirk = True,
+        has_event_write_sample_count = True,
+        load_inline_uniforms_via_preamble_ldgk = True,
+        load_shader_consts_via_preamble = True,
+        ubwc_unorm_snorm_int_compatible = True,
         supports_ibo_ubwc = True,
     )
 
