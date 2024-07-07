c8bbe5b0ced98d2d1d9895aff47950fe210b6903
Vera-Firefly <87926662+Vera-Firefly@users.noreply.github.com>
2024-07-07 17:46:35 +0800

    Update freedreno_devices.py

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -802,16 +802,13 @@ a7xx_730 = A7XXProps(
     )
 
 a7xx_735 = A7XXProps(
-        #cmdbuf_start_a725_quirk = True,
+        cmdbuf_start_a725_quirk = True,
         stsc_duplication_quirk = True,
         has_event_write_sample_count = True,
-        load_inline_uniforms_via_preamble_ldgk = True,
-        load_shader_consts_via_preamble = True,
-        #has_gmem_vpc_attr_buf = True,
-        #sysmem_vpc_attr_buf_size = 0x20000,
-        #gmem_vpc_attr_buf_size = 0xc000,
         ubwc_unorm_snorm_int_compatible = True,
-        #supports_ibo_ubwc = True,
+        supports_ibo_ubwc = True,
+        fs_must_have_non_zero_constlen_quirk = True,
+        enable_tp_ubwc_flag_hint = False,
     )
 
 a7xx_740 = A7XXProps(
